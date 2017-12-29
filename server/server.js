7f1498bded9480730dc9b78d722ecf3cd35a98d0
Jatin Kumar <jatn15011999@gmail.com>
2017-12-29 17:44:12 +0530

    Add connect and disconnect handellers

--- server/server.js
+++ server/server.js
@@ -2,14 +2,26 @@
  * Created by HP on 29-12-2017.
  */
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO  = require('socket.io');
 
 var app = express();
 const port = process.env.PORT||3000;
 const publicPath = path.join(__dirname, '../public');
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(port,()=>{
+io.on('connection',(socket)=>{
+   console.log('New user connected!');
+    socket.on('disconnect',(socket)=>{
+        console.log('User disconnected!');
+    });
+});
+
+
+server.listen(port,()=>{
     console.log(`Server is up on PORT : ${port}`);
 });
